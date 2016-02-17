19de7dff438fc3c24cce1c6627900cc24b04fbe6
Joey Zheng <Zhaowei@Joeys-MacBook-Air.local>
2016-02-16 20:37:37 -0800

    Added basic tables

---
+++ database_script/init/initialize_base.sql
@@ -0,0 +1,115 @@
+DROP SCHEMA IF EXISTS `Before_I_Die`;
+CREATE SCHEMA `Before_I_Die`;
+
+USE `Before_I_Die`;
+
+DROP TABLE IF EXISTS `Users`;
+CREATE TABLE `Users`
+(
+  `ID` BIGINT(64) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `Username` VARCHAR(50) NOT NULL,
+  `FirstName` VARCHAR(50) NOT NULL,
+  `LastName` VARCHAR(50) NOT NULL,
+  `Salt` VARCHAR(256) NOT NULL,
+  `Password` VARCHAR(256) NOT NULL,
+  `PasswordAttempt` TINYINT(1) NULL DEFAULT 0,
+  `Locked` BIT(1) NULL DEFAULT 0,
+  `Status` BIT(1) NULL DEFAULT 1,
+  PRIMARY KEY (`ID`),
+  UNIQUE INDEX `Username_UNIQUE` (`Username` ASC)
+);
+
+DROP TABLE IF EXISTS `HashTag`;
+CREATE TABLE `HashTag`
+(
+  `ID` BIGINT(64) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `HashTag` VARCHAR(30) NOT NULL,
+  `Status` BIT(1) NULL DEFAULT 1,
+  PRIMARY KEY (`ID`),
+  UNIQUE INDEX `HashTag_UNIQUE` (`HashTag` ASC)
+);
+
+DROP TABLE IF EXISTS `BucketItem`;
+CREATE TABLE `BucketItem`
+(
+  `ID` BIGINT(64) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `Title` VARCHAR(100) NOT NULL,
+  `Content` VARCHAR(2000) NULL,
+  `CompleteTime` DATETIME NULL,
+  `Location` VARCHAR(200) NULL,
+  `Private` BIT(1) NULL DEFAULT 1,
+  `CreateDate` DATETIME NOT NULL,
+  `BucketListID` BIGINT(64) UNSIGNED NOT NULL,
+  `Status` BIT(1) NULL DEFAULT 1,
+  PRIMARY KEY (`ID`)
+);
+
+DROP TABLE IF EXISTS `BucketItemHashTag`;
+CREATE TABLE `BucketItemHashTag`
+(
+  `ID` BIGINT(64) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `BucketItemID` BIGINT(64) UNSIGNED NOT NULL,
+  `HashTagID` BIGINT(64) UNSIGNED NOT NULL,
+  `Status` BIT(1) NULL DEFAULT 1,
+  PRIMARY KEY (`ID`),
+  INDEX `fk_BucketItemHashTag_BucketItemID_idx` (`BucketItemID` ASC),
+  INDEX `fk_BucketItemHashTag_HashTagID_idx` (`HashTagID` ASC),
+  CONSTRAINT `fk_BucketItemHashTag_BucketItemID`
+    FOREIGN KEY (`BucketItemID`)
+    REFERENCES `Before_I_Die`.`BucketItem` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_BucketItemHashTag_HashTagID`
+    FOREIGN KEY (`HashTagID`)
+    REFERENCES `Before_I_Die`.`HashTag` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+DROP TABLE IF EXISTS `BucketList`;
+CREATE TABLE `BucketList`
+(
+  `ID` BIGINT(64) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `UserID` BIGINT(64) UNSIGNED NOT NULL,
+  `OwnerID` BIGINT(64) UNSIGNED NOT NULL,
+  `CreateDate` DATETIME NOT NULL,
+  `Status` BIT(1) NULL DEFAULT 1,
+  PRIMARY KEY (`ID`),
+  INDEX `fk_BucketList_UserID_idx` (`UserID` ASC),
+  INDEX `fk_BucketList_OwnerID_idx` (`OwnerID` ASC),
+  CONSTRAINT `fk_BucketList_UserID`
+    FOREIGN KEY (`UserID`)
+    REFERENCES `Before_I_Die`.`Users` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_BucketList_OwnerID`
+    FOREIGN KEY (`OwnerID`)
+    REFERENCES `Before_I_Die`.`Users` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+DROP TABLE IF EXISTS `BucketListBucketItem`;
+CREATE TABLE `Before_I_Die`.`BucketListBucketItem`
+(
+  `ID` BIGINT(64) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `BucketListID` BIGINT(64) UNSIGNED NOT NULL,
+  `BucketItemID` BIGINT(64) UNSIGNED NOT NULL,
+  `Status` BIT(1) NULL DEFAULT 1,
+  PRIMARY KEY (`ID`),
+  INDEX `fk_BucketListBucketItem_BucketListID_idx` (`BucketListID` ASC),
+  INDEX `fk_BucketListBucketItem_BucketItemID_idx` (`BucketItemID` ASC),
+  CONSTRAINT `fk_BucketListBucketItem_BucketListID`
+    FOREIGN KEY (`BucketListID`)
+    REFERENCES `Before_I_Die`.`BucketList` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_BucketListBucketItem_BucketItemID`
+    FOREIGN KEY (`BucketItemID`)
+    REFERENCES `Before_I_Die`.`BucketItem` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+
+
