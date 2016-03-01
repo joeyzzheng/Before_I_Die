fa0aa78f2ed73c7ee1f5ddb0889b55b8d0aa6a05
ahsyuyu <ahsyuyu@gmail.com>
2016-03-01 11:18:02 -0800

    dynamically render bucket list

---
+++ resource/js/b.js
@@ -0,0 +1,38 @@
+var bucket_list = [
+	{
+		"title":"first item",
+		"image":"https://upload.wikimedia.org/wikipedia/commons/3/3e/Salar_uyuni_200701.jpg",
+		"description":"this is description",
+		"like":2,
+		"inherited":false,
+		"comment":[
+			{
+				"name":"user1",
+				"text":"gooood!"
+			},
+			{
+				"name":"user2",
+				"text":"nice!"
+			}
+		]
+	},
+
+	{
+		"title":"second item",
+		"image":"http://www.divezonebath.co.uk/images/bkground_diver.jpg",
+		"description":"this is description",
+		"like":3,
+		"inherited":false,
+		"comment":[
+			{
+				"name":"user3",
+				"text":"comment2!"
+			},
+			{
+				"name":"user4",
+				"text":"comment2!"
+			}
+		]
+	}
+
+];
---
+++ resource/js/bucket_list.js
@@ -0,0 +1,42 @@
+console.log(bucket_list[0].comment[0].name);
+
+var render_bucket_item = function(i) {
+	//render image and menu
+	$(".bucket-items").append('<div class="bucket-item" data-item="' + i + '"></div>');
+	$(".bucket-item[data-item='" + i + "']").append('<div class="item-img-section" data-item="' + i + '"></div>');
+	var img = "<img class='item-img alt='bucket item image' src='" + bucket_list[i].image + "'>";
+	$(".item-img-section[data-item='" + i + "']").append(img);
+	var menu = '<div class="dropdown">   <button class="dropbtn"><img class="menu" alt="menu" src="../resource/pic/menu.png"></button>   <div class="dropdown-content"> 	<a href="#">Edit</a> 	<a href="#">Completed</a> 	<a href="#">Request Relay</a> 	<a href="#">Delete</a> 	  <div class="sub-dropdown"> 	  	<a href="#">Privacy</a> 	  		<div class="dropdown-sub-content"> 		  		<a href="#">Private</a> 			  	<a href="#">Public</a> 		  	</div>   	</div>   </div> </div>';
+	
+	$(".item-img-section[data-item='" + i + "']").append(menu);
+	
+	
+	//render title des
+	$(".bucket-item[data-item='" + i + "']").append('<div class="item-info" data-item="' + i + '"></div>');
+	var title = "<h2>" + bucket_list[i].title + "</h2>";
+	$(".item-info[data-item='" + i + "']").append(title);
+	var description = '<div class="item-desc">' + bucket_list[i].description + '</div>';
+	$(".item-info[data-item='" + i + "']").append(description);
+	$(".item-info[data-item='" + i + "']").append("<hr>");
+	
+	//render response
+	$(".item-info[data-item='" + i + "']").append('<div class="response" data-item="' + i + '"></div>');
+	$(".response[data-item='" + i + "']").append('<div class="like" data-item="' + i + '"></div>');
+	var like = '<img class="icon-like" alt="like button" src="../resource/pic/like.png"><span class="count-of-like">' + bucket_list[i].like + '</span><img class="torch" src="../resource/pic/torch.png" alt="torch>">';
+	$(".like[data-item='" + i + "']").append(like);
+	
+	//render comment
+	for(var j = 0; j < bucket_list[i].comment.length; j++) {
+		$(".response[data-item='" + i + "']").append('<p class="comment"><span class="comment"><b>' + bucket_list[i].comment[j].name + '</b></span>' + bucket_list[i].comment[j].text + '</p>');
+	}
+	
+	
+	
+}
+
+var render = function() {
+	for(var i = 0 ; i < bucket_list.length; i++) {
+		render_bucket_item(i);
+	}
+	
+}
