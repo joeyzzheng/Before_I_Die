9875cd3dd8ff86980fc24476bb9c0de7aed1e818
Joey Zheng <Zhaowei@Joeys-MacBook-Air.local>
2016-03-05 15:43:06 -0800

    before changing to develop branch

---
+++ database_script/update/1/RecentTorchSelect.sql
@@ -0,0 +1,12 @@
+USE `Before_I_Die`;
+DROP PROCEDURE IF EXISTS `RecentTorchSelect`;
+
+DELIMITER //
+USE `Before_I_Die`//
+CREATE PROCEDURE `RecentTorchSelect` ()
+BEGIN
+        
+
+            
+END//
+DELIMITER ;
