511376cd16a8f5d6fe89b8d2fc9af561b14f5784
Joey Zheng <Zhaowei@Joeys-MacBook-Air.local>
2016-03-06 15:17:07 -0800

    deleted resource folder

--- resource/js/test_ajax.js
+++
@@ -1,8 +0,0 @@
-
-$.ajax({url: "https://apiapache-beforeidie.rhcloud.com/api/bucketlist/joeyzheng", success: function(result){
-        console.log(result);
-}});
-
-// $.ajax({url: "https://apiapache-beforeidie.rhcloud.com/api/bucket_item/complete", success: function(result){
-//         console.log(result);
-// }});
